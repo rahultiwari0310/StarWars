46528c1e99d687b57f49e5ecaa6a01b29afa79e5
Rahul Tiwari <rahul.tiwari2@avizva.com>
2017-12-09 00:20:34 +0530

    Minor changes.

--- __test__/Login.test.jsx
+++ __test__/Login.test.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from '../js/components/Login.jsx';
+import sinon from 'sinon';
+import Login from 'components/Login';
+import { expect } from 'chai';
+import ReactTestUtils from 'react-dom/test-utils'; // ES6
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('Login', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Login/>, div);
+    it('Login renders without crashing', () => {
+
+        const login = Enzyme.mount( React.createElement( Login )),
+            inputUsername = login.find( '#username' ),
+            inputPassword = login.find( '#password'),
+            error = login.find( '.hide'),
+            submitButton = login.find( '.form__button' );
+
+        inputUsername.value = 'Luke Skywalker';
+        inputPassword.value = '19BB';
+
+        login.handleSubmit = sinon.spy();
+
+        expect( error ).to.have.length(1);
+
+        inputUsername.simulate( 'change', inputUsername );
+        inputPassword.simulate( 'change', inputPassword );
+        submitButton.simulate( 'click' );
+
+        expect( inputUsername ).to.have.length(1);
+        expect( inputPassword ).to.have.length(1);
+        expect( submitButton ).to.have.length(1);
+
     });
 });
